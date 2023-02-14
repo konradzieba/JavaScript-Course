8acc412262316a4bc178534b7aa27577596acd72
Konrad Zięba <162624@student.uwm.edu.pl>
2023-02-14 13:01:20 +0100

    added query selectors and pop up after clicking ADD btn

---
+++ Projects/06 - Notes/script.js
@@ -0,0 +1,26 @@
+const addBtn = document.querySelector('.add');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+const deleteBtns = document.getElementsByClassName('delete-note');
+const noteArea = document.querySelector('.note-area');
+const notePanel = document.querySelector('.note-panel');
+const deleteAllBtn = document.querySelector('.delete-all')
+const category = document.querySelector('#category');
+const textarea = document.querySelector('#text');
+const error = document.querySelector('.error');
+let selectedValue;
+let cardId = 0;
+
+const openPanel = () => {
+    notePanel.style.display = 'flex';
+}
+
+const closePanel = () => {
+    notePanel.style.display = 'none';
+    error.style.visibility = 'hidden';
+    textarea.value = '';
+    category.selectedIndex = 0;
+}
+
+addBtn.addEventListener('click', openPanel)
+cancelBtn.addEventListener('click', closePanel)
