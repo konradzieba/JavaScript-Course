ae9a7db811af004db4aa1d91528035acb0d7738e
Konrad Zięba <162624@student.uwm.edu.pl>
2023-02-15 14:26:18 +0100

    Date Countdown App - complete

--- Projects/09 - Date Countdown App/script.js
+++ Projects/09 - Date Countdown App/script.js
@@ -15,7 +15,38 @@ const secondsCount = document.querySelector('.seconds-count');
 
 const saveBtn = document.querySelector('.save');
 const eventSpan = document.querySelector('.event');
+let userTime;
+
+const setTime = () => {
+    const currentTime = new Date();
+    const result = userTime - currentTime;
+
+    const days = Math.floor(result / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(result / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(result / 1000 / 60) % 60;
+    const seconds = Math.floor(result / 1000) % 60;
+
+    daysCount.textContent = days;
+    hoursCount.textContent = hours;
+    minutesCount.textContent = minutes;
+    secondsCount.textContent = seconds;
+}
+
+const appUpdate = () => {
+    eventSpan.textContent = eventName.value;
+    
+    userTime = new Date(`${eventMonth.value} ${eventDay.value}, ${eventYear.value} 00:00:00`);
+
+    imageSection.style.backgroundImage = `url('${eventImg.value}')`;
+    // setTime();
+}
+
+appUpdate();
+setInterval(setTime, 1000);
 
 settingsBtn.addEventListener('click', () => {
     settings.classList.toggle('active');
 })
+
+
+saveBtn.addEventListener('click', appUpdate)
